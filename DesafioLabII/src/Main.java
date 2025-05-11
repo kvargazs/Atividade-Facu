37527030504e0c266c738867d110077e48c9f67c
Karoline <133781521+kvargazs@users.noreply.github.com>
2025-05-11 16:54:57 -0300

    Delete DesafioLabII/src/Main.java

--- DesafioLabII/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Labirinto l = new Labirinto();
-        l.criaLabirinto("src/labirinto.txt"); //caminho do arquivo do labirinto
-        l.imprimirLabirinto();
-
-        System.out.println("Existe saída? " + l.percorreLabirinto());
-    }
-}
