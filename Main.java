426b12147c792b80705e47df0e6d1e6c6e1595ed
Karoline <133781521+kvargazs@users.noreply.github.com>
2025-05-11 16:46:44 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Labirinto l = new Labirinto();
-        l.criaLabirinto("src/labirinto.txt");
-        l.imprimirLabirinto();
-
-        System.out.println("Existe saída? " + l.percorreLabirinto());
-    }
-}
